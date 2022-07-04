df656e121c9537677e39d97514eb18e5b561437c
Ana Gomes Souza <agomesou@emeal.nttdata.com>
2022-07-04 09:40:45 -0300

    Desenvolvendo SplashScreen

--- app/src/main/java/com/example/harraypotterapp/ui/splashScreen/SplashScreen.kt
+++ app/src/main/java/com/example/harraypotterapp/ui/splashScreen/SplashScreen.kt
@@ -1,11 +1,19 @@
 package com.example.harraypotterapp.ui.splashScreen
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.harraypotterapp.MainActivity
+import com.example.harraypotterapp.R
 
 class SplashScreen : AppCompatActivity() {
+    private val time: Long = 3000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+        android.os.Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, time)
     }
 }
