fe7a811e12a5470c349067e7761e78afa0999a83
Ana Claudia Gomes Souza <103140224+annecgs@users.noreply.github.com>
2022-12-12 08:35:57 -0300

    Update URL

--- backend/src/main/java/com/example/backend/utils/Constants.kt
+++ backend/src/main/java/com/example/backend/utils/Constants.kt
@@ -2,6 +2,7 @@ package com.example.backend.utils
 
 class Constants {
     companion object {
-        const val BASE_URL = "http://hp-api.herokuapp.com/api/"
+        //const val BASE_URL = "http://hp-api.herokuapp.com/api/"
+        const val BASE_URL = "https://hp-api.onrender.com/api/"
     }
 }
